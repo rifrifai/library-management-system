8a6b1fc1705c29210179afe7be2c541d8122af0c
rifaiabdurrohman <rifaiabdurrohman8@gmail.com>
2025-10-06 14:10:25 +0700

    feat : endpoint movie finished

---
+++ library-management-system-api/Migrations/20251006070327_DeleteUserAndBorrorTransaction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace library_management_system_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteUserAndBorrorTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BorrowTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BorrowTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BookId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    BorrowDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    ReturnDate = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BorrowTransactions_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BorrowTransactions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowTransactions_BookId",
+                table: "BorrowTransactions",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowTransactions_UserId",
+                table: "BorrowTransactions",
+                column: "UserId");
+        }
+    }
+}
